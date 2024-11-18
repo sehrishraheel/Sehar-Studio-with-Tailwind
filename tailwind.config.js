c7ea3142664500468b9ff1e330873ab1c23818e3
sehrishraheel <sehrishm322@gmail.com>
2024-11-18 17:09:28 +0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+      extend: {  primary: "#FF6F61", // Sehar Studio's custom primary color
+                 secondary: "#FFD700", // Accent color
+                 background: "#FFF5F3", // Light background shade
+                 textPrimary: "#4A4A4A", // Text color},
+  },
+  plugins: [],
+},
+
+};
